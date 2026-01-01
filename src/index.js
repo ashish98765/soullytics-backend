d6e1d1a131f36ada667954880435337e3c0f2ac1
ashish98765 <32891451+ashish98765@users.noreply.github.com>
2026-01-01 18:05:44 +0530

    connect health route

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
-
+const healthRoutes = require("./routes/health.routes");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // middleware
 app.use(express.json());
 
+app.use("/health", healthRoutes);
 // test route
 app.get("/", (req, res) => {
   res.send("Soullytics backend is running");
