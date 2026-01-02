ea0c7c8c2e8b5909f8ff52f1a51c734cc28ed39c
ashish98765 <32891451+ashish98765@users.noreply.github.com>
2026-01-02 16:32:42 +0530

    metaAds services

---
+++ src/services/metaAds.service.js
@@ -0,0 +1,9 @@
+exports.createMetaAd = ({ objective, budget }) => {
+  return {
+    platform: "meta",
+    creative: {
+      headline: "Get More Leads Today",
+      description: "High quality leads for your business using smart targeting"
+    }
+  };
+};
