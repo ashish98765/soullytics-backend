c2d05502af0075cf95df8385a2bf2b42c40d7668
ashish98765 <32891451+ashish98765@users.noreply.github.com>
2026-01-02 17:55:06 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,20 @@
 const express = require("express");
 const app = express();
 
-// middleware
-app.use(express.json());
-
-// routes
 const adsRoutes = require("./routes/ads.routes");
 const decisionRoutes = require("./routes/decision.routes");
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
 
 app.use("/ads", adsRoutes);
 app.use("/decision", decisionRoutes);
 
-// root
 app.get("/", (req, res) => {
   res.send("Soullytics backend running");
 });
 
-// port
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(Server running on port ${PORT});
 });
