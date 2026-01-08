9e56b848c56ead1676969d68ceaa058fced59938
ashish98765 <32891451+ashish98765@users.noreply.github.com>
2026-01-08 14:43:39 +0530

    Delete src/routes/ads.routes.js

--- src/routes/ads.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { createAd } = require("../controllers/ads.controller");
-
-router.post("/create", createAd);
-
-module.exports = router;
