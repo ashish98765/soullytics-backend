5cbe96bcc18d0704fe459d93228d3597d7d3f5c5
ashish98765 <32891451+ashish98765@users.noreply.github.com>
2026-01-08 14:43:54 +0530

    Delete src/routes/decision.routes.js

--- src/routes/decision.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { decide } = require("../controllers/decision.controller");
-
-router.post("/decide", decide);
-
-module.exports = router;
