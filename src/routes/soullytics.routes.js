1380e4f089b1886761fba08f29f319874e2bfb97
ashish98765 <32891451+ashish98765@users.noreply.github.com>
2026-01-08 15:05:51 +0530

    Update soullytics.routes.js

--- src/routes/soullytics.routes.js
+++ src/routes/soullytics.routes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 const {
   handleSoullytics
-} = require("../controllers/soullytics.controller");
+} = require("../controllers/soullytics.controller.js");
 
 // Single Soullytics entry point
 router.post("/soullytics", handleSoullytics);
