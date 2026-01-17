c2fc0606a515b2f7cd8718e1c3050db7c492f990
ashish98765 <32891451+ashish98765@users.noreply.github.com>
2026-01-17 16:42:35 +0530

    Update objectiveBudget.engine.js

--- src/core/objectiveBudget.engine.js
+++ src/core/objectiveBudget.engine.js
@@ -1,17 +1,19 @@
-const { ObjectiveClarityEngine } = require("../adsCode01.objectiveClarity");
-const { BudgetRealityEngine } = require("../adsCode02.budgetReality");
+// src/core/objectiveBudget.engine.js
 
-class ObjectiveBudgetEngine {
-  constructor(context) {
-    this.context = context;
-  }
+const objectiveClarity = require("../engines/adsCode01.objectiveClarity");
+const budgetReality = require("../engines/adsCode02.budgetReality");
 
-  run() {
-    return [
-      new ObjectiveClarityEngine(this.context).run(),
-      new BudgetRealityEngine(this.context).run()
-    ];
-  }
+function run(context = {}) {
+  const objectiveResult = objectiveClarity.run(context);
+  const budgetResult = budgetReality.run(context);
+
+  return {
+    valid:
+      objectiveResult.status !== "FAIL" &&
+      budgetResult.status !== "FAIL",
+    objective: objectiveResult,
+    budget: budgetResult
+  };
 }
 
-module.exports = { ObjectiveBudgetEngine };
+module.exports = { run };
