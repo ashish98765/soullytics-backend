a6f5c6da27a57f07421951fc32ef497180131bdf
ashish98765 <32891451+ashish98765@users.noreply.github.com>
2026-01-17 16:28:19 +0530

    Update dataFusionEngine.js

--- src/core/dataFusionEngine.js
+++ src/core/dataFusionEngine.js
@@ -1,19 +1,17 @@
-// src/core/dataFusionEngine.js
-
-const adsCode00 = require('../engines/adsCode00.dataFusion');
-const adsCode24 = require('../engines/adsCode24.signalQuality');
-const adsCode35 = require('../engines/adsCode35.dataReliability');
+const adsCode00 = require("../engines/adsCode00.dataFusion");
+const adsCode24 = require("../engines/adsCode24.signalQuality");
+const adsCode35 = require("../engines/adsCode35.dataReliability");
 
 async function dataFusionEngine(context = {}) {
-  const signals = [];
+  const results = [];
 
-  if (adsCode00) signals.push(adsCode00(context));
-  if (adsCode24) signals.push(adsCode24(context));
-  if (adsCode35) signals.push(adsCode35(context));
+  if (adsCode00) results.push(adsCode00(context));
+  if (adsCode24) results.push(adsCode24(context));
+  if (adsCode35) results.push(adsCode35(context));
 
   return {
-    trusted: signals.every(s => s?.status !== 'FAIL'),
-    signals
+    trusted: results.every(r => r.status !== "FAIL"),
+    results
   };
 }
 
